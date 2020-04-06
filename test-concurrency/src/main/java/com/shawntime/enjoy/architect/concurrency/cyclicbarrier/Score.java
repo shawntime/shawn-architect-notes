04b768f2c2ae4ec2abc794b8db974359fe9c65b8
mashaohua <mashaohua@autohome.com.cn>
2020-04-06 17:34:10 +0800

    CyclicBarrier

---
+++ test-concurrency/src/main/java/com/shawntime/enjoy/architect/concurrency/cyclicbarrier/Score.java
@@ -0,0 +1,34 @@
+package com.shawntime.enjoy.architect.concurrency.cyclicbarrier;
+
+public class Score {
+
+    private String sportsmanName;
+
+    private int distance;
+
+    private int time;
+
+    public String getSportsmanName() {
+        return sportsmanName;
+    }
+
+    public void setSportsmanName(String sportsmanName) {
+        this.sportsmanName = sportsmanName;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+}
