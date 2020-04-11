080974a33a24f655b05fc2b6d0419566a2e10c68
mashaohua <mashaohua@autohome.com.cn>
2020-04-11 13:24:12 +0800

    Semaphore信号量实现互斥锁

---
+++ test-concurrency/src/main/java/com/shawntime/enjoy/architect/concurrency/semaphore/mutex/Mutex.java
@@ -0,0 +1,23 @@
+package com.shawntime.enjoy.architect.concurrency.semaphore.mutex;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * semaphore实现互斥锁
+ */
+public class Mutex {
+
+    private Semaphore semaphore = new Semaphore(1, true);
+
+    public void acquire() throws InterruptedException {
+        semaphore.acquire();
+    }
+
+    public void release() {
+        semaphore.release();
+    }
+
+    public boolean attempt(int ms) throws InterruptedException {
+        return semaphore.tryAcquire(ms);
+    }
+}
