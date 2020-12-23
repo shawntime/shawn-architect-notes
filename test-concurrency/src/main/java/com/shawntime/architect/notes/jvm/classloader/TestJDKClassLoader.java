932193be13665c813c70bf7395be77ade46805b1
mashaohua <mashaohua@autohome.com.cn>
2020-12-23 18:28:05 +0800

    测试类加载器

---
+++ test-concurrency/src/main/java/com/shawntime/architect/notes/jvm/classloader/TestJDKClassLoader.java
@@ -0,0 +1,42 @@
+package com.shawntime.architect.notes.jvm.classloader;
+
+public class TestJDKClassLoader {
+
+    /**
+     * null
+     * sun.misc.Launcher$ExtClassLoader
+     * sun.misc.Launcher$AppClassLoader@18b4aac2
+     * sun.misc.Launcher$AppClassLoader
+     */
+    public static void main(String[] args) {
+        // 启动类加载器：由C++实现，classLoader为null
+        System.out.println(String.class.getClassLoader());
+        // 扩展类加载器：sun.misc.Launcher$ExtClassLoader
+        System.out.println(com.sun.crypto.provider.DESKeyFactory.class.getClassLoader().getClass().getName());
+        // 应用类加载器：sun.misc.Launcher$AppClassLoader@18b4aac2
+        System.out.println(TestJDKClassLoader.class.getClassLoader());
+        System.out.println(ClassLoader.getSystemClassLoader().getClass().getName());
+
+        ClassLoader classLoader = TestJDKClassLoader.class.getClassLoader();
+        printClassLoader(classLoader);
+
+    }
+
+    /**
+     * sun.misc.Launcher$AppClassLoader@18b4aac2
+     * sun.misc.Launcher$ExtClassLoader@7ea987ac
+     * null
+     */
+    private static void printClassLoader(ClassLoader classLoader) {
+        System.out.println("打印类的classLoader...");
+        System.out.println(classLoader);
+        do {
+            classLoader = classLoader.getParent();
+            System.out.println(classLoader);
+        } while (classLoader != null);
+    }
+
+    public void print(Class<?> clazz) {
+        printClassLoader(clazz.getClassLoader());
+    }
+}
